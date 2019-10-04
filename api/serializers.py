ede96efb16783c61ada34821dc3967539d242741
Gustavo Pereira de Almeida <contato@gustavohalm.com>
2019-10-04 15:35:10 -0300

    Serializers Complete

--- api/serializers.py
+++ api/serializers.py
@@ -16,8 +16,28 @@ class FarmSerializer(serializers.ModelSerializer):
         read_only_fields = ['id',]
 
 
-class Agricultural(serializers.ModelSerializer):
+class AgriculturalSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Agricultural
+        fields = '__all__'
+        read_only_fields = ['id', ]
+
+
+class PartnershipAgriculturalSerializer(serializers.ModelSerializer):
+    agricultural = AgriculturalSerializer(many=False)
+    farmer = FarmerSerializer(many=False)
+
+    class Meta:
+        model = models.PartnershipAgricultural
+        fields = '__all__'
+        read_only_fields = ['id',]
+
+
+class PartnershipFarmSerializer(serializers.ModelSerializer):
+    farm =  FarmSerializer(many=False)
+    agricultural = AgriculturalSerializer(many = False, allow_null=True)
+    farmer = FarmerSerializer(many= False, allow_null=True)
+    class Meta:
+        model = models.PartnershipFarm
         fields = '__all__'
         read_only_fields = ['id', ]
